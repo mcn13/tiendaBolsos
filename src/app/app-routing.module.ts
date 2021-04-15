c2eafbe12f9796985c3905562f80116982ffa9e4
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-04-15 14:19:47 +0200

    creamos la rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PasarelaComponent } from './pages/pasarela/pasarela.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Pagina principal
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full',
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
+  },
+  {
+    path: 'pasarela',
+    component: PasarelaComponent,
+  },
+  {
+    path: 'productos',
+    component: ProductosComponent,
+  },
+  {
+    path: 'detalles-productos/:id',
+    component: DetalleProductoComponent,
+  },
+  // si ponen cualquier url
+  {
+    path: '**',
+    redirectTo: 'productos',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
