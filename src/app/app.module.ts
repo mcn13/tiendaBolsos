ee64589c4b5c53936510a1f9a91bae0c492befc3
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-04-15 12:37:31 +0200

    keys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const config = {
+  apiKey: 'AIzaSyAvZhm3aJT2DdF3Wy4HFfsYlNbEJzMN104',
+  authDomain: 'tienda-bolsos.firebaseapp.com',
+  projectId: 'tienda-bolsos',
+  storageBucket: 'tienda-bolsos.appspot.com',
+  messagingSenderId: '1020550770465',
+  appId: '1:1020550770465:web:2c74126da53cd1cc471860',
+  measurementId: 'G-SRHJ03BNQY',
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
