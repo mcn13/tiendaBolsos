249c55a281d68da586213dccd0b32ad708ab4898
mcn13 <marinacambeiro98@gmail.com>
2021-04-28 12:49:39 +0200

    guardar favoritos array localStorage

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -10,134 +10,161 @@ selector: 'app-productos',
 templateUrl: './productos.component.html',
 styleUrls: ['./productos.component.scss']
 })
+
+
 export class ProductosComponent implements OnInit {
 
-productos: producto[] = []
-productosMostrar: producto[] = []
+    productos: producto[] = [];
+    productosMostrar: producto[] = [];
+    elementosFavoritos= ( localStorage.getItem('elementosFavoritos') )? localStorage.getItem('elementosFavoritos').split(',') : [] ;
+    // existe este elemento en el localStorage ? Si existe me lo asignas a esta propiedad
 
-elementosFavoritos = [];
+    // si no existe le asignamos un empty array
 
-constructor(
-private db: AngularFirestore,
-private router: Router,
-private cestaServ: CestaService
-) {
-}
+    // si existe el string, tendre que hacer algo hay un metodo aplicable alos strings,
+    // que ese string me lo va a
+    // convertir a formato array
 
-selectFavorite(producto: producto){
-console.log('seleccionado', producto);
-this.elementosFavoritos.push( producto);
-console.log('ELEMENTOS FAVORITOS', this.elementosFavoritos)
-}
+    constructor(
 
-deselectFavorite(producto){
-console.log('desselect produ', producto);
-console.log('ID A DESSELECCIONAR', producto.url)
+      private db: AngularFirestore,
+      private router: Router,
+      private cestaServ: CestaService
+      ){
+    }
 
-const index = this.elementosFavoritos.findIndex(producto);
-console.log('INDEX', index)
-this.elementosFavoritos.splice(index, 1)
+    selectFavorite(producto: producto){
 
+      (this.elementosFavoritos.indexOf(producto.url) >= 0) ? null : this.elementosFavoritos.push( producto.url);
+      localStorage.setItem('elementosFavoritos', this.elementosFavoritos.toString())
+      console.log(this.elementosFavoritos);
 
-}
+    }
 
+    deselectFavorite(producto){
 
+        console.log('desselect produ', producto);
+        console.log('ID A DESSELECCIONAR', producto.url)
 
+        const index = this.elementosFavoritos.indexOf(producto.url);
 
-filtrarProductos(filtro: Filtro){
-console.log('filtro que viene del hijo', filtro);
+        if ( index >= 0) {
+          console.log('INDEX');
+          this.elementosFavoritos.splice(index, 1)
+          localStorage.setItem('elementosFavoritos', this.elementosFavoritos.toString())
+        } else {
 
-/// filtrar primero el texto
-const arrayFiltrandoTexto = this.filtrarTexto( this.productos, filtro);
-
-/// filtro el precio
-const arrayFiltrandoPrecio = this.filtrarPrecio( arrayFiltrandoTexto, filtro);
-
-/// filtro el color
-const arrayFiltrandoColor = this.filtrarColor( arrayFiltrandoPrecio, filtro);
-
-/// filtro el tipo
-const arrayFiltrandoTipo = this.filtrarTipo( arrayFiltrandoColor, filtro);
+        }
 
 
-this.productosMostrar = [... arrayFiltrandoTipo];
 
+        console.log(this.elementosFavoritos);
 
-}
+    }
 
 
 
 
-filtrarTexto(array: producto[], filtro: Filtro) : producto[]{
+    filtrarProductos(filtro: Filtro){
+
+        console.log('filtro que viene del hijo', filtro);
+
+        /// filtrar primero el texto
+        const arrayFiltrandoTexto = this.filtrarTexto( this.productos, filtro);
+
+        /// filtro el precio
+        const arrayFiltrandoPrecio = this.filtrarPrecio( arrayFiltrandoTexto, filtro);
+
+        /// filtro el color
+        const arrayFiltrandoColor = this.filtrarColor( arrayFiltrandoPrecio, filtro);
+
+        /// filtro el tipo
+        const arrayFiltrandoTipo = this.filtrarTipo( arrayFiltrandoColor, filtro);
 
 
-const texto = filtro.texto;
+        this.productosMostrar = [... arrayFiltrandoTipo];
 
-// no es verdad , o que no existe , o que es null, o que es false; -1, '', null, undefined,
 
-if(! texto){
-return array
-} else {
-return array.filter((producto: producto)=>{
-const nombre = producto.nombre.toLowerCase().trim()
-return nombre.includes(texto.toLowerCase().trim());
-})
-}
-}
+    }
 
-filtrarPrecio(array: producto[], filtro: Filtro): producto[]{
-console.log('FILTRO PRECIO', filtro)
-const precioMaximo = filtro.precio.precioMaximo;
-const precioMinimo = filtro.precio.precioMinimo;
 
-return array.filter((producto: producto)=>{
-const precioDeEsteProducto = this.cestaServ.precioFinal(producto);
-return ( precioDeEsteProducto > precioMinimo ) && ( precioDeEsteProducto < precioMaximo)
-})
-}
 
-filtrarColor(array: producto[], filtro: Filtro): producto[]{
-const color = filtro.color
-if( !color || ( color === 'todos' ) ){ // si el color es igual a 'todos'
-return array // no apliques ningun filtro
-}else{
-return array.filter((producto: producto)=>{
-const arrayDeColoresDisponibles = producto.colores;
-return arrayDeColoresDisponibles.includes(color)
-})
-}
-}
 
-filtrarTipo(array: producto[], filtro: Filtro): producto[]{
-const tipo = filtro.tipo; // si el tipo es 'todos'
-if( !tipo || (tipo === 'todos') ){ // no apliques ningun filtro
-return array
-}else{
-return array.filter((producto:producto)=>{
-return producto.tipo === tipo
-})
-}
-}
+    filtrarTexto(array: producto[], filtro: Filtro) : producto[]{
 
-ngOnInit(): void {
-this.db.collection('productos').valueChanges().subscribe(( res )=>{
-this.productos = res as producto[];
-this.filtrarProductos({
-precio:{
-precioMaximo: localStorage.getItem('precioMaximo') ? parseInt(localStorage.getItem('precioMaximo')) : 0,
-precioMinimo: localStorage.getItem('precioMinimo') ? parseInt(localStorage.getItem('precioMinimo')) : 100
-},
-tipo: localStorage.getItem('tipo') ? localStorage.getItem('tipo') : 'todos',
-color: localStorage.getItem('color') ? localStorage.getItem('color') : 'todos',
-texto: localStorage.getItem('texto') ? localStorage.getItem('texto') : null,
-});
+        const texto = filtro.texto;
 
-})
-}
+        // no es verdad , o que no existe , o que es null, o que es false; -1, '', null, undefined,
 
-navegar(i){
-console.log('navegar', i);
-this.router.navigate([ 'detalle-producto', i ])
-}
+        if(! texto){
+            return array
+        } else {
+            return array.filter((producto: producto)=>{
+            const nombre = producto.nombre.toLowerCase().trim()
+            return nombre.includes(texto.toLowerCase().trim());
+            })
+        }
+    }
+
+    filtrarPrecio(array: producto[], filtro: Filtro): producto[]{
+
+        console.log('FILTRO PRECIO', filtro)
+        const precioMaximo = filtro.precio.precioMaximo;
+        const precioMinimo = filtro.precio.precioMinimo;
+
+        return array.filter((producto: producto)=>{
+            const precioDeEsteProducto = this.cestaServ.precioFinal(producto);
+            return ( precioDeEsteProducto > precioMinimo ) && ( precioDeEsteProducto < precioMaximo)
+    })
+    }
+
+    filtrarColor(array: producto[], filtro: Filtro): producto[]{
+
+        const color = filtro.color
+        if( !color || ( color === 'todos' ) ){ // si el color es igual a 'todos'
+            return array // no apliques ningun filtro
+        }else{
+            return array.filter((producto: producto)=>{
+            const arrayDeColoresDisponibles = producto.colores;
+            return arrayDeColoresDisponibles.includes(color)
+            })
+        }
+    }
+
+    filtrarTipo(array: producto[], filtro: Filtro): producto[]{
+
+        const tipo = filtro.tipo; // si el tipo es 'todos'
+        if( !tipo || (tipo === 'todos') ){ // no apliques ningun filtro
+            return array
+        }else{
+            return array.filter((producto:producto)=>{
+            return producto.tipo === tipo
+            })
+        }
+    }
+
+    ngOnInit(): void {
+
+        this.db.collection('productos').valueChanges().subscribe(( res )=>{
+
+            this.productos = res as producto[];
+            this.filtrarProductos({
+                precio:{
+                precioMaximo: localStorage.getItem('precioMaximo') ? parseInt(localStorage.getItem('precioMaximo')) : 0,
+                precioMinimo: localStorage.getItem('precioMinimo') ? parseInt(localStorage.getItem('precioMinimo')) : 100
+                },
+                tipo: localStorage.getItem('tipo') ? localStorage.getItem('tipo') : 'todos',
+                color: localStorage.getItem('color') ? localStorage.getItem('color') : 'todos',
+                texto: localStorage.getItem('texto') ? localStorage.getItem('texto') : null,
+            });
+
+        })
+    }
+
+    navegar(i){
+
+        console.log('navegar', i);
+        this.router.navigate([ 'detalle-producto', i ])
+    }
 
 }
